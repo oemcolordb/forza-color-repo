a115767cfa59d3b04597d7c32995829525777075
xblackxscars123 <xblackxscars123@gmail.com>
2025-10-04 02:36:59 +1300

    Fix missing apple-touch-icon error

--- app/layout.js
+++ app/layout.js
@@ -141,7 +141,7 @@ export default function RootLayout({ children }) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/icon.svg" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(structuredData) }}
