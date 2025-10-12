f7db6a68cc595af6b641305e481c00d302149111
xblackxscars123 <xblackxscars123@gmail.com>
2025-10-13 09:13:49 +1300

    Update Google site verification meta tag

--- app/layout.js
+++ app/layout.js
@@ -137,7 +137,7 @@ export default function RootLayout({ children }) {
 
 
 
-        <meta name="google-site-verification" content="ce0ac2383b2a111a" />
+        <meta name="google-site-verification" content="vG2Z9j6nstH8oDSGfxfICIrbefBCUu0cIttuSxMIiOk" />
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
