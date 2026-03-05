69cc6f3372b85336811fe69f1ea30e33480776e2
LEADEV <xblackxscars123@gmail.com>
2026-03-06 06:07:07 +1300

    chore: remove dynamic export and revalidation settings from page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 'use client'
 
-export const dynamic = 'force-dynamic'
-export const revalidate = 0
-
 import React, { useState, useEffect, useMemo, useCallback } from 'react'
 import { CarColor, DeviceInfo, ExtractedColor } from './types'
 import { ErrorBoundary } from './lib/errorBoundary'
